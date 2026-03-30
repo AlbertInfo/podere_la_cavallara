7d376e5127721b1df0230a4c15090e265af17ace
AlbertInfo <alb.stend97@gmail.com>
2026-03-30 11:59:54 +0200

    revisione_admin2

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -9,6 +9,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/it.js"></script>
-    <script src="<?= e(admin_url('assets/js/admin-ui.js')) ?>?v=4"></script>
+    <script src="<?= e(admin_url('assets/js/admin-ui.js')) ?>?v=5"></script>
 </body>
 </html>
