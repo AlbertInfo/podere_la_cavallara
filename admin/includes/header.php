e8272c5dfbf52b7431e2269269cdaed059aff004
AlbertInfo <alb.stend97@gmail.com>
2026-04-04 14:21:38 +0200

    test_possibility_new_feature_admin6

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -29,7 +29,7 @@ function admin_nav_active(array $targets, string $currentPath): string
 
 <body>
     <div class="admin-app">
-        <?php if ($currentAdmin): ?>
+         <?= $currentAdmin ? 'has-sidebar' : 'auth-layout' ?>
             <aside class="admin-sidebar" id="adminSidebar">
                 <a class="sidebar-brand" href="<?= e(admin_url('index.php')) ?>">
                     <img class="sidebar-logo sidebar-logo-desktop" src="<?= e(admin_url('assets/img/logo_sticky.png')) ?>" alt="Podere La Cavallara">
