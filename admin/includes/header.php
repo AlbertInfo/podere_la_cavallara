40a3ac1c963ae41708517fe308d3aa85c0e0ba96
AlbertInfo <alb.stend97@gmail.com>
2026-04-06 19:10:47 +0200

    fix_select-booking

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -33,7 +33,7 @@ function admin_nav_active(array $targets, string $currentPath): string
             <aside class="admin-sidebar" id="adminSidebar">
                 <a class="sidebar-brand" href="<?= e(admin_url('index.php')) ?>">
                     <img class="sidebar-logo sidebar-logo-desktop" src="<?= e(admin_url('assets/img/logo.svg')) ?>" alt="Podere La Cavallara">
-                    <img class="sidebar-logo sidebar-logo-mobile" src="<?= e(admin_url('assets/img/logo_mobile.svg')) ?>" alt="Podere La Cavallara">
+                    <img class="sidebar-logo sidebar-logo-mobile" src="<?= e(admin_url('assets/img/logo.svg')) ?>" alt="Podere La Cavallara">
                 </a>
 
                 <nav class="sidebar-nav" aria-label="Menu area admin">
