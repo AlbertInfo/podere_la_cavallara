2a28586ca28248fd44f6784a52bc0f8a4b6b20a3
AlbertInfo <alb.stend97@gmail.com>
2026-04-14 08:56:34 +0200

    restyling_mobile13

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -78,6 +78,7 @@ $mobilePageKicker = admin_mobile_page_kicker($currentPath);
     <title><?= e($pageTitle) ?></title>
     <link rel="stylesheet" href="/admin/assets/css/admin-modern.css?v=47">
     <link rel="stylesheet" href="/admin/assets/css/interhome-import.css?v=95">
+    <link rel="stylesheet" href="/admin/assets/css/admin-mobile.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
