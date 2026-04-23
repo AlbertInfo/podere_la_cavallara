8b7cf6e2a24cca2d2844b8977bbf3d0d190567ec
AlbertInfo <alb.stend97@gmail.com>
2026-04-23 13:47:42 +0200

    imlement-ocr-documentsx

--- admin/includes/document-ocr-config.php
+++ admin/includes/document-ocr-config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'enabled' => true,
     'endpoint' => 'https://eu-documentai.googleapis.com/v1/projects/104713903040/locations/eu/processors/196275ef2624638a:process',
-    'credentials_path' => '/home/u881781553/domains/poderelacavallara.it/config/client_secret_104713903040-aq4s63e62dpokhkq2lgbmt999oc7r34i.apps.googleusercontent.com.json',
+    'credentials_path' => '/home/u881781553/domains/poderelacavallara.it/config/scandocuments-494116-c9617789be44.json',
     'bearer_token' => null,
     'timeout_seconds' => 90,
     'max_file_size_bytes' => 10 * 1024 * 1024,
